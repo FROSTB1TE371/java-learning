2fd39b5ef6b063dad034ad88d815fa4657e62b61
ЛKonstantins.Morozs <konstantinsmorozs@gmail.com>
2024-11-14 21:33:33 +0200

    [feature/java_lesson_2]: Loops, practice, ArraysExample

---
+++ src/main/java/lv/acodemy/Practice.java
@@ -0,0 +1,18 @@
+package lv.acodemy;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(5 * i);
+        }
+    }
+}
